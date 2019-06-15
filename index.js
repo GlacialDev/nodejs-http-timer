bd39e2136c07f2898f309617a613e0356e0afaed
Glacial <kli-van@yandex.ru>
2019-06-15 17:13:04 +0300

    env variables; good date format

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 const http = require("http");
 const port = 3000;
 
-const period = 10000;
-const interval = 1000;
-
 http
   .createServer((req, res) => {
+    const period = +process.env.Glacial_period;
+    const interval = +process.env.Glacial_interval;
+    // console.log(period, interval);
+
     res.setHeader("Content-Type", "text/html; charset=utf-8;");
 
-    console.log(toUTC(new Date()) + " запрос пришел в это время"), interval;
+    console.log(getNiceTime(new Date()) + " запрос пришел в это время"),
+      interval;
     let intervalId = setInterval(() => {
-      console.log(toUTC(new Date())), interval;
+      console.log("" + getNiceTime(new Date())), interval;
     }, interval);
 
     setTimeout(() => {
       clearInterval(intervalId);
-      console.log(toUTC(new Date()) + " остановка работы таймера");
-      res.write(
-        "текущая дата и время отключения в формате UTC: " + toUTC(new Date())
-      );
+      console.log(getNiceTime(new Date()) + " остановка работы таймера");
+      res.write("текущая дата и время отключения: " + getNiceTime(new Date()));
       res.end();
     }, period);
   })
   .listen(port);
 
-const toUTC = date => {
-  return Date.UTC(
-    date.getUTCFullYear(),
-    date.getUTCMonth(),
-    date.getUTCDate(),
-    date.getUTCHours(),
-    date.getUTCMinutes(),
-    date.getUTCSeconds()
-  );
+const getNiceTime = date => {
+  const normalize = number => {
+    if (number < 10) {
+      return "0" + number;
+    } else {
+      return number;
+    }
+  };
+
+  let hh = normalize(date.getHours());
+  let mm = normalize(date.getMinutes());
+  let ss = normalize(date.getSeconds());
+
+  let day = normalize(date.getDay());
+  let month = normalize(date.getMonth());
+  let year = date.getFullYear();
+
+  return `${day}.${month}.${year} в ${hh}:${mm}:${ss}`;
 };
